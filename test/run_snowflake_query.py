696f4cc1addfcdd481bf2e633df0bc04065a0339
natenorton-ifit <nate.norton@ifit.com>
2025-07-09 16:05:31 -0600

    Update run_snowflake_query.py

--- test/run_snowflake_query.py
+++ test/run_snowflake_query.py
@@ -6,7 +6,7 @@ print("Account value:", os.environ['SNOWFLAKE_ACCOUNT'])  # Add this
 conn = snowflake.connector.connect(
     user=os.environ['SNOWFLAKE_USER'],
     password=os.environ['SNOWFLAKE_PASSWORD'],
-    account=os.environ['SNOWFLAKE_ACCOUNT'],
+    account='myxccrc-uja24426.us-east-1',
     warehouse='REPORTING_WAREHOUSE',
     database='ANALYTICS',
     schema='ANALYTICS_QUALITY',
